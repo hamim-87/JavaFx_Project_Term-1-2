751831a76b2ee4ac695f94db89f2e60fedf21435
hamim-87 <hamim12109087@gmail.com>
2023-09-10 23:36:58 +0600

    half done

--- FoodHunter/src/main/java/Controllers/HomePageController.java
+++ FoodHunter/src/main/java/Controllers/HomePageController.java
@@ -2,6 +2,7 @@ package Controllers;
 
 import Client.MyListListener;
 import Comunications.FileOperation;
+import DataBaseSystem.Food;
 import DataBaseSystem.Restaurant;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -66,12 +67,19 @@ public class HomePageController implements Initializable {
     @FXML
     private GridPane gridRestaurant;
 
+    @FXML
+    private GridPane Foodgrid;
+
     private MyListListener myListListener;
 
 
     @FXML
     private AnchorPane RestaurantCard;
     private List<Restaurant> RestaurantList = new ArrayList<>();
+
+
+    //FOOD
+    private List<Food> FoodList = new ArrayList<>();
 
     public  void setRestaurantList(List<Restaurant> restaurantList)
     {
@@ -128,7 +136,7 @@ public class HomePageController implements Initializable {
 
                 AnchorPane dummy = new AnchorPane();
                 dummy.setMaxWidth(400);
-                dummy.setMaxHeight(70);
+                dummy.setMaxHeight(40);
 
 
                 if(i == 0)
@@ -157,14 +165,56 @@ public class HomePageController implements Initializable {
             throw new RuntimeException(e);
         }
     }
+
+
+    public void showFoods() throws IOException {
+        FileOperation fo = new FileOperation();
+
+        FoodList = fo.ReadFileForMenu();
+
+
+        int colom = 0,row = 0;
+        for (int i = 0;i<FoodList.size();i++)
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/FxmlFiles/Food.fxml"));
+
+
+            AnchorPane anchorPane = fxmlLoader.load();
+
+            FoodCardController foodCardController = fxmlLoader.getController();
+
+            foodCardController.setDataForFood(FoodList.get(i));
+
+            Foodgrid.add(anchorPane,colom,row++);
+
+            //width
+            Foodgrid.setMinWidth(Region.USE_COMPUTED_SIZE);
+            Foodgrid.setPrefWidth(Region.USE_COMPUTED_SIZE);
+            Foodgrid.setMaxWidth(Region.USE_PREF_SIZE);
+
+            //height
+            Foodgrid.setMinHeight(Region.USE_COMPUTED_SIZE);
+            Foodgrid.setPrefHeight(Region.USE_COMPUTED_SIZE);
+            Foodgrid.setMaxHeight(Region.USE_PREF_SIZE);
+
+            GridPane.setMargin(anchorPane,new Insets(10));
+
+
+        }
+
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //Test
 
         ShowRestaurants();
-
-
+        try {
+            showFoods();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
